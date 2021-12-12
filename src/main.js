4932ce32c5e51657a4c444f4386080b7ce7b06cc
Mahdi <prowebsi998@gmail.com>
2021-12-12 22:09:03 +0330

    Create a project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import "./registerServiceWorker";
+import "./style.css";
+
+createApp(App).use(store).use(createPinia()).use(router).mount("#app");
